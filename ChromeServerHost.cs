f98e9a104ff3c356f739f799763941a97519a1c8
Brett Hewitt <brett_hewitt@live.co.uk>
2019-02-09 22:09:21 +0000

    Initial Commit

---
+++ ChromeServerHost.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NativeMessaging;
+using Newtonsoft.Json.Linq;
+
+namespace dataDyneChromeServerHost
+{
+    public class MessageReceievedArgs : EventArgs
+    {
+        public JObject Data
+        {
+            get;
+            set;
+        }
+    }
+
+    public delegate void MessageReceivedHandler(object sender, MessageReceievedArgs args);
+
+    public class ChromeServerHost : Host
+    {
+        public event MessageReceivedHandler MessageReceived;
+
+        public override string Hostname
+        {
+            get { return "com.datadyne.chromeserver.message"; }
+        }
+
+        public ChromeServerHost()
+        {
+
+        }
+
+        protected override void ProcessReceivedMessage(JObject data)
+        {
+            if (MessageReceived != null)
+            {
+                MessageReceived(this, new MessageReceievedArgs() {Data = data});
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using HostClientCommunication;
+using NativeMessaging;
+using Newtonsoft.Json.Linq;
+
+namespace dataDyneChromeServerHost
+{
+    class Program
+    {
+        private static ChromeServerHost Host;
+        
+        private static bool ChromeConnectionStable = true;
+        private static NamedPipeServerStream PipeServer;
+
+        static void Main(string[] args)
+        {
+            Host = new ChromeServerHost();
+            Host.LostConnectionToChrome += Host_LostConnectionToChrome;
+            Thread serverThread = new Thread(ServerThread);
+            serverThread.Start();
+            Thread.Sleep(250);
+                
+            Host.Listen();
+        }
+
+        private static void Host_LostConnectionToChrome(object sender, EventArgs e)
+        {
+            ChromeConnectionStable = false;
+            ResetEvent.Set();
+        }
+
+        static void ProcessConnection(IAsyncResult result)
+        {
+            try
+            {
+                PipeServer.EndWaitForConnection(result);
+
+                // Read the request from the client. Once the client has
+                // written to the pipe its security token will be available.
+
+                ServerCommunication serverCommunication = new ServerCommunication(PipeServer);
+
+                // Verify our identity to the connected client using a
+                // string that the client anticipates.
+
+                serverCommunication.SendMessage("dataDyne Chrome Server");
+
+                string command = serverCommunication.ReadMessage();
+                var j = new ResponseConfirmation(command);
+
+                bool waitForReponse = true;
+                bool processResponse = true;
+                JObject reply = null;
+                Host.MessageReceived += (s, a) =>
+                {
+                    //Handle Response
+                    if (processResponse)
+                    {
+                        reply = a.Data;
+                    }
+
+                    waitForReponse = false;
+                };
+                Host.SendMessage(j.GetJObject());
+
+                Stopwatch sw = new Stopwatch();
+                sw.Start();
+                while (waitForReponse)
+                {
+                    if (sw.ElapsedMilliseconds == (10 * 1000))
+                    {
+                        //We've timed out
+                        waitForReponse = false;
+                        processResponse = false;
+                    }
+                }
+
+                string response = "";
+                if (processResponse && reply != null)
+                {
+                    //Send reply back
+                    response = reply.ToString();
+                }
+                else
+                {
+                    //Reponse failed
+                    response = "{\"text\": \"Failed to communicate with chrome extension\"}";
+                }
+
+                PipeServer.RunAsClient(() => serverCommunication.SendMessage(response));
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("ERROR: {0}", e.Message);
+            }
+            
+            PipeServer.Close();
+
+            ProcessFinishedEvent.Set();
+        }
+        
+        private static ManualResetEvent ResetEvent;
+        private static ManualResetEvent ProcessFinishedEvent;
+
+        private static void ServerThread(object data)
+        {
+            ResetEvent = new ManualResetEvent(false);
+            
+            while (true)
+            {
+                PipeServer = new NamedPipeServerStream("dataDyneChromeServerPipe", PipeDirection.InOut, 1, PipeTransmissionMode.Message, PipeOptions.Asynchronous);
+                ProcessFinishedEvent = new ManualResetEvent(false);
+                // Wait for a client to connect
+                PipeServer.BeginWaitForConnection(ProcessConnection, PipeServer);
+                
+                int result = WaitHandle.WaitAny(new[] { ResetEvent, ProcessFinishedEvent });
+
+                if (!ChromeConnectionStable || result == 0)
+                {
+                    PipeServer.Close();
+                    break;
+                }
+            }
+        }
+    }
+}
